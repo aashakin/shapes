7de5f2c950bebcdb0a5bf255d07559fdc4f0aebd
Alexander Shakin <alexandr.shakin@gmail.com>
2020-03-17 18:09:42 +0300

    Refactored TriangleTests

--- Shapes/ShapesTests/TriangleTests.cs
+++ Shapes/ShapesTests/TriangleTests.cs
@@ -10,11 +10,11 @@ namespace ShapesTests
         [TestCase(3.0, 4.0, 5.0, 6.0)]
         [TestCase(1.0, 1.0, 1.0, 0.433012701)]
         [TestCase(5.0, 7.0, 8.0, 17.320508075)]
-        public void ShouldCalculateAreaIfTriangleIsValid(double sideA, double sideB, double sideC, double area)
+        public void CalculateArea_TriangleIsValid_ReturnArea(double sideA, double sideB, double sideC, double expectedValue)
         {
             var triangle = new Triangle(sideA, sideB, sideC);
 
-            Assert.That(triangle.CalculateArea(), Is.EqualTo(area).Within(Numbers.Epsilon));
+            Assert.That(triangle.CalculateArea(), Is.EqualTo(expectedValue).Within(Numbers.Epsilon));
         }
 
         [TestCase(-1.0, 1.0, 1.0)]
@@ -23,26 +23,22 @@ namespace ShapesTests
         [TestCase(0.0, 1.0, 1.0)]
         [TestCase(1.0, 0.0, 1.0)]
         [TestCase(1.0, 1.0, 0.0)]
-        public void ShouldThrowExceptionIfSideIsLessThanOrEqualToZero(double sideA, double sideB, double sideC)
+        public void CreateTriangle_SideIsLessThanOrEqualToZero_ThrowException(double sideA, double sideB, double sideC)
         {
-            var ex = Assert.Throws<ArgumentException>(() => new Triangle(sideA, sideB, sideC));
-
-            Assert.IsNotNull(ex);
+            Assert.That(() => new Triangle(sideA, sideB, sideC), Throws.ArgumentException);
         }
         
         [TestCase(1.0, 3.0, 1.0)]
         [TestCase(1.0, 3.0, 2.0)]
-        public void ShouldThrowExceptionIfTriangleIsNotValid(double sideA, double sideB, double sideC)
+        public void CreateTriangle_TriangleIsNotValid_ThrowException(double sideA, double sideB, double sideC)
         {
-            var ex = Assert.Throws<ArgumentException>(() => new Triangle(sideA, sideB, sideC));
-
-            Assert.IsNotNull(ex);
+            Assert.That(() => new Triangle(sideA, sideB, sideC), Throws.ArgumentException);
         }
         
         [TestCase(3.0, 4.0, 5.0)]
         [TestCase(3.0, 5.0, 4.0)]
         [TestCase(5.0, 4.0, 3.0)]
-        public void ShouldReturnTrueIfTriangleIsRightTriangle(double sideA, double sideB, double sideC)
+        public void IsRightTriangle_TriangleIsRightTriangle_ReturnTrue(double sideA, double sideB, double sideC)
         {
             var triangle =  new Triangle(sideA, sideB, sideC);
 
@@ -50,7 +46,7 @@ namespace ShapesTests
         }
         
         [TestCase(3.0, 5.0, 5.0)]
-        public void ShouldReturnFalseIfTriangleIsNotRightTriangle(double sideA, double sideB, double sideC)
+        public void IsRightTriangle_TriangleIsNotRightTriangle_ReturnFalse(double sideA, double sideB, double sideC)
         {
             var triangle =  new Triangle(sideA, sideB, sideC);
 
